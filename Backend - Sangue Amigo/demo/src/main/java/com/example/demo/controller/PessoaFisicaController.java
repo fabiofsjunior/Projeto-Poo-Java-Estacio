6bbaf3ee0dde8f9e145101863aac9e3d305a4cb5
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2024-03-16 18:35:11 -0300

    add - GET endpoint PessoaFisica

--- Backend - Sangue Amigo/demo/src/main/java/com/example/demo/controller/PessoaFisicaController.java
+++ Backend - Sangue Amigo/demo/src/main/java/com/example/demo/controller/PessoaFisicaController.java
@@ -1,4 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.PessoaFisicaEntity;
+import com.example.demo.service.PessoaFisicaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "*")
 public class PessoaFisicaController {
+
+    @Autowired
+    private PessoaFisicaService pessoaFisicaService;
+
+    @GetMapping("/api/pessoafisica")
+    public List<PessoaFisicaEntity> buscarUsuario() {
+        return pessoaFisicaService.listarDoadoresPf();
+    }
+
 }
